19feb64b94f84fb62774694dea040abe798c6e6f
Vikram <vikram@Vikrams-MacBook-Pro.local>
2023-05-18 17:51:41 -0300

    defined function, varables lowercase, uppercase, numeric, and special ccharacters. Added prompt for password length and cofirm password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,30 @@
 // Assignment code here
 
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numericNum = "0123456789";
+var specialChar = "!@#$%^&*()_+=~`[]{}|\\:;'<>,.?/";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+var passField = document.getElementById('password');
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function writePassword() {
+  var passLength = parseInt(prompt("Enter length of the password at least 8 and no more than 128:"));
+}
+
+if (passLength<8 || passlength>128) {
+  alert("invalid #, enter between 8 and 128 only");
+  return;
+}
+
+var enterlower = confirm ("include lowercase characters in your password?");
+var enterUpper = confirm ("Include uppercase characters in your password?");
+var enterNumeric = confirm ("Include numberic characters in your password?");
+var enterSpecial = confirm ("include special characters in your password?");
+
