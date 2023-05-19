1578da609d6fd6185a70e9cdff9c47d206af5079
Vikram <vikram@Vikrams-MacBook-Pro.local>
2023-05-19 10:07:25 -0300

    created the function to generate password and code is functioning as expected

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,68 @@
 // Assignment code here
 
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var numericNum = "0123456789";
-var specialChar = "!@#$%^&*()_+=~`[]{}|\\:;'<>,.?/";
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 var passField = document.getElementById('password');
 
 
-// Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
 
 function writePassword() {
-  var passLength = parseInt(prompt("Enter length of the password at least 8 and no more than 128:"));
+  var length = parseInt(prompt("Password Criteria: Enter a '#' - between 8 and 128:"));
+
+  if (length < 8 || length > 128) {
+    alert("enter between 8 and 128 only");
+    return;
+  }
+
+  var enterLower = confirm("Password Criteria to Include lowercase?");
+  var enterUpper = confirm("Password Criteria to Include uppercase?");
+  var enterNumeric = confirm("Password Criteria to Include numeric?");
+  var enterSpecial = confirm("Password Criteria to Include special characters?");
+
+
+  if (!enterLower && !enterUpper && !enterNumeric && !enterSpecial) {
+    alert("At least one character type should be selected !");
+    return;
+  }
+
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericNum = "0123456789";
+  var specialChar = "!@#$%^&*()_+=~`[]{}|\\:;'<>,.?/";
+
+  var validChars = "";
+
+
+  if (enterLower) {
+    validChars += lowerCase;
+  }
+
+  if (enterUpper) {
+    validChars += upperCase;
+  }
+
+  if (enterNumeric) {
+    validChars += numericNum;
+  }
+
+  if (enterSpecial) {
+    validChars += specialChar;
+  }
+
+  var password = "";
+
+
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * validChars.length);
+    password += validChars.charAt(randomIndex);
+  }
+
+  passField.value = password;
+
 }
 
-if (passLength<8 || passlength>128) {
-  alert("invalid #, enter between 8 and 128 only");
-  return;
-}
 
-var enterlower = confirm ("include lowercase characters in your password?");
-var enterUpper = confirm ("Include uppercase characters in your password?");
-var enterNumeric = confirm ("Include numberic characters in your password?");
-var enterSpecial = confirm ("include special characters in your password?");
 
